7b25fa12154aff48e88e0534dcca3d23368c4fd3
Technicallyaustin <Technicallyaustin@gmail.com>
2020-06-28 16:29:55 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+    get '/newteam' do
+        erb :newteam
+    end
+
+    get '/team' do
+        erb :team 
+    end
+
+    post '/team' do
+    puts params.to_s
+    erb :team
+    end
+
+
+
 
 
 end
